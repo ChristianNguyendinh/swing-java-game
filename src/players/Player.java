92a0890b54c28375a96cd38a375e013feb59f2e4
Christian <christian@Christians-MacBook-Air.local>
2016-04-09 01:23:00 -0400

    Updating player

--- src/players/Player.java
+++ src/players/Player.java
@@ -5,11 +5,40 @@ import characters.Character;
 
 public class Player {
 	
+	private String name;
+	private int totalHP;
+	private int totalATK;
 	private boolean myTurn;
 	private Character[] characters;
 	
-	public Player() {
+	public Player(String name) {
+		this.name = name;
+		totalHP = 30;
+		totalATK = 30;
 		myTurn = false;
 		characters = new Character[6];
+	}
+	
+	public boolean addCharacter(Character character) {
+		for(int i = 0; i < 6; i++) {
+			if(characters[i] == null) {
+				characters[i] = character;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void incrementAtk(Character character) {
+		
+	}
+	
+	public String action(Player otherPlayer, Character yours, Character theirs) {
+		theirs.setHP(theirs.getHP() - yours.getATK());
+		//Abilities
+		if(yours instanceof Character) {
+			return "String ";
+		}
+		return "";
 	}
 }
